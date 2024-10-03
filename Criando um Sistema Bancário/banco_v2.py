9e0a2f033bd42a91f28248c3e61f682b4cf0fb83
ricardo.ferreira <ricardo.ferreira@fgv.br>
2024-10-03 15:22:17 -0300

    Desafio quase finalizado

--- Criando um Sistema Bancário/banco_v2.py
+++ Criando um Sistema Bancário/banco_v2.py
@@ -4,10 +4,12 @@ Separar as funções existentes de saque, depósito e extrato em funções. Cria
 
 
 Desafio:
-Precisamos deixar nosso código mais modularizado, para isso vamos criar funções para as operações existentes: sacar, depositar e visualizar histórico. Além disso, para a versão 2 do nosso sistema precisamos criar duas novas funções: criar usuário (cliente do banco) e criar conta corrente (vincular com usuário).
+Precisamos deixar nosso código mais modularizado, para isso vamos criar funções para as operações existentes: sacar, depositar e visualizar histórico. Além disso, 
+para a versão 2 do nosso sistema precisamos criar duas novas funções: criar usuário (cliente do banco) e criar conta corrente (vincular com usuário).
 
 Saque: ✔
-A função saque deve receber os argumentos apenas por nome (keyword only). Sugestão de argumentos: saldo, valor, extrato, limite, numero_saques, limite_saques. Sugestão de retorno: saldo e extrato.
+A função saque deve receber os argumentos apenas por nome (keyword only). Sugestão de argumentos: saldo, valor, extrato, limite, numero_saques, limite_saques. 
+Sugestão de retorno: saldo e extrato.
 
 Depósito: ✔
 A função depósito deve receber os argumentos apenas por posição (positional only). Sugestão de argumentos: saldo, valor, extrato. Sugestão de retorno: saldo e extrato.
@@ -15,14 +17,16 @@ A função depósito deve receber os argumentos apenas por posição (positional
 Extrato: ✔
 A função extrato deve receber os argumentos por posição e nome (positional only e keyword only). Argumentos posicionais: saldo, argumentos nomeados: extrato.
 
-Novas funções:
+Novas funções: ✔
 Precisamos criar duas novas funções: criar usuário e criar conta corrente. Fique a vontade para adicionar mais funções, exemplo: listar contas.
 
-Criar usuário (cliente):
-O programa deve armazenar os usuários em uma lista, um usuário é composto por: nome, data de nascimento, cpf e endereço. O endereço é uma string com o formato: logradouro, nro bairro cidade/sigla estado. Deve ser armazenado somente os números do CPF. Não podemos cadastrar 2 usuários com o mesmo CPF.
+Criar usuário (cliente): ✔
+O programa deve armazenar os usuários em uma lista, um usuário é composto por: nome, data de nascimento, cpf e endereço. O endereço é uma string com o formato: logradouro, 
+nro bairro cidade/sigla estado. Deve ser armazenado somente os números do CPF. Não podemos cadastrar 2 usuários com o mesmo CPF.
 
-Criar conta corrente:
-O programa deve armazenar contas em uma lista, uma conta é composta por: agência, número da conta e usuário. O número da conta é sequencial, iniciando em 1. O número da agência é fixo: "0001". O usuário pode ter mais de uma conta, mas uma conta pertence a somente um usuário.
+Criar conta corrente: ✔
+O programa deve armazenar contas em uma lista, uma conta é composta por: agência, número da conta e usuário. O número da conta é sequencial, iniciando em 1. 
+O número da agência é fixo: "0001". O usuário pode ter mais de uma conta, mas uma conta pertence a somente um usuário.
 
 Dica:
 Para vincular um usuário a uma conta, filtre a lista de usuários buscando o número do CPF informado para cada usuário da lista.
@@ -34,9 +38,10 @@ def menu():
             [d] Depositar
             [s] Sacar
             [e] Extrato
-           [cc] Criar conta
+           [cc] Criar conta corrente
            [lc] Listar contas
            [nu] Novo usuário
+           [lu] Listar usuários
             [q] Sair
 
            ======================================
@@ -96,11 +101,58 @@ def extrato_bancario(saldo, /, *, extrato):
     print("\n=========================================")
 
 
+def cadastrar_usuario(lista_usuarios):
+    cpf = int(input("Digite seu CPF: "))
+
+    if cpf not in lista_usuarios:
+        nome = input("Digite seu nome: ")
+        data_nascimento = input("Digite sua data de nascimento: ")
+        endereco = input("Digite seu endereço: ")
+        lista_usuarios[cpf] = {"nome": nome, "data_nascimento": data_nascimento, "endereco": endereco}
+
+    elif cpf in lista_usuarios:
+        print("CPF já cadastrado!")
+
+    return lista_usuarios
+
+
+def criar_conta(lista_usuario, lista_contas, AGENCIA, numero_conta):
+    cpf = int(input("Digite seu CPF: "))
+
+    if cpf in lista_usuario:
+        numero_conta = numero_conta + 1
+        lista_contas = lista_contas + f"""
+Nome:    {lista_usuario[cpf]["nome"]}
+Agência: {AGENCIA}
+Conta:   {numero_conta}
+\n========================================
+        """
+    
+    elif cpf not in lista_usuario:
+        print("Este usuário não possui cadastro, favor se cadastrar!")
+        cadastrar_usuario(lista_usuario)
+    
+    return lista_contas, numero_conta
+
+
+def listar_contas(lista_contas):
+    print("\n================ CONTAS ================")
+    print("Não há contas cadastradas. \n========================================" if not lista_contas else lista_contas)
+
+def listar_usuarios(lista_usuarios):
+    print("\n=============== USUÁRIOS ===============")
+    print("Não há usuários cadastrados. \n========================================" if not lista_usuarios else lista_usuarios)
+
+
 def main():
     LIMITE_SAQUES = 3
     numero_atual_saques = 0
     saldo = 2000
     extrato = ""
+    lista_usuarios = {}
+    lista_contas = """"""
+    AGENCIA = "0001"
+    numero_conta = 0
 
     while True:
         opcao = menu()
@@ -118,6 +170,19 @@ def main():
         elif opcao == "q":
             print("Saindo...")
             break
+        
+        elif opcao == "nu":
+            print("Iniciando cadastro...")
+            cadastrar_usuario(lista_usuarios)
+        
+        elif opcao == "lu":
+            listar_usuarios(lista_usuarios)
+
+        elif opcao == "cc":
+            lista_contas, numero_conta = criar_conta(lista_usuarios, lista_contas, AGENCIA, numero_conta)
+
+        elif opcao == "lc":
+            listar_contas(lista_contas)
 
         else:
             print("Opção inválida")
--- Dicionários - criação e acesso aos dados/criacao_e_acesso_dados.py
+++ Dicionários - criação e acesso aos dados/criacao_e_acesso_dados.py
@@ -36,16 +36,16 @@ contatos = {
   "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}, 
   "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"}, 
   "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"}, 
-  "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"}, 
+  "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
 }
 
-telefone = contatos ["giovanna@gmail.com"]["telefone"] # "3443-2121"
+telefone = contatos["giovanna@gmail.com"]["telefone"] # "3443-2121"
 print(telefone)
 
 # ==================================================================
 
-for chave in contatos:
-  print(chave, contatos[chave])
+# for chave in contatos:
+#   print(chave, contatos[chave])
 
-for chave, valor in contatos.items(): # O método .items() retorna uma lista de tuplas
-  print(chave, valor)
+# for chave, valor in contatos.items(): # O método .items() retorna uma lista de tuplas
+#   print(chave, valor)
