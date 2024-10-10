3dd38403522ded1ea32ca6a00474e3f6065c6639
Ricardo <ricardo.gferreira97@hotmail.com>
2024-10-09 21:38:10 -0300

    finalizando heranças

--- Conceitos de Herança/heranca_multipla.py
+++ Conceitos de Herança/heranca_multipla.py
@@ -6,14 +6,14 @@ class Animal:
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
 class Mamifero(Animal):
-    def __init__(self, numero_patas):
-        self.numero_patas = numero_patas
-        super().__init__(numero_patas)
+    def __init__(self, cor_pelo, **kw):
+        self.cor_pelo = cor_pelo
+        super().__init__(**kw)
 
 class Ave(Animal):
-    def __init__(self, numero_patas):
-        self.numero_patas = numero_patas
-        super().__init__(numero_patas)
+    def __init__(self, cor_bico, **kw):
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
 
 class Cachorro(Mamifero):
     pass
@@ -22,6 +22,15 @@ class Gato(Mamifero):
     pass
 
 class Ornitorrinco(Mamifero, Ave):
-    pass
+    def __init__(self, cor_bico, cor_pelo, numero_patas):
+        print(Ornitorrinco.__mro__)
 
-gato = Gato(4)
+        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, numero_patas=numero_patas)
+
+# gato = Gato(4, "laranja")  <- Erro
+gato = Gato(numero_patas=4, cor_pelo="laranja")
+print(gato)
+
+pery = Ornitorrinco(numero_patas=4, cor_pelo="verde", cor_bico="laranja")
+print(pery)
+
